0943d1986cdb3cf6dd23edbe8af967faaa8de9fd
Damien <dlortiethibaut@gmail.com>
2024-06-11 22:11:00 +0200

    add middleware, token with cookies and init statistics

--- backend/src/routes/companyRoute.js
+++ backend/src/routes/companyRoute.js
@@ -4,21 +4,25 @@ const multer = require("multer");
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 const CompanyController = require("../controllers/companyController");
+const middleware = require("../middleware/middleware");
 
 // Create an company (POST)
-router.post("/add", CompanyController.add);
+router.post("/add", middleware.authenticator, CompanyController.add);
 
 // Delete company (DELETE)
-router.post("/delete/:id", CompanyController.delete);
+router.delete(
+  "/delete/:id",
+  middleware.authenticator,
+  CompanyController.delete
+);
 
 // Get all company (GET)
-router.post("/all", CompanyController.getAll);
+router.get("/all", middleware.authenticator, CompanyController.getAll);
 
 // Get company by ID (GET)
-router.get("/get/:id", CompanyController.getById);
+router.get("/get/:id", middleware.authenticator, CompanyController.getById);
 
 // Update company (PUT)
-router.put("/update/:id", CompanyController.update);
-
+router.put("/update/:id", middleware.authenticator, CompanyController.update);
 
 module.exports = router;
--- frontend/src/components/Aside/Aside.tsx
+++ frontend/src/components/Aside/Aside.tsx
@@ -1,40 +1,102 @@
-import React, { useState, useEffect } from 'react'
-import AsideItem from '../AsideItem/AsideItem'
-import { useAuth } from '../../contexts/AuthContext'
-import { Cloud, Lightning, UploadSimple , Sliders, Cube, User, X } from '@phosphor-icons/react'
+import React, { useState, useEffect } from "react";
+import AsideItem from "../AsideItem/AsideItem";
+import { useAuth } from "../../contexts/AuthContext";
+import {
+  Cloud,
+  Lightning,
+  UploadSimple,
+  Sliders,
+  Cube,
+  User,
+  X,
+  ChartLine,
+} from "@phosphor-icons/react";
 
 export default function Aside() {
-
-  const { handleLogout } = useAuth()
-  const [active, setActive] = useState('storage' as string)
+  const { handleLogout } = useAuth();
+  const [active, setActive] = useState("storage" as string);
 
   useEffect(() => {
-    setActive(window.location.pathname.split('/')[1])
-  }, [window.location.pathname])
+    setActive(window.location.pathname.split("/")[1]);
+  }, [window.location.pathname]);
 
   const handleChangeItem = (item: string) => {
-    setActive(item)
-  }
+    setActive(item);
+  };
 
   return (
     <React.Fragment>
-        <div className="flex flex-col my-4 mx-4">
-          <div className='title flex flex-row justify-center md:justify-between items-center'>
-            <h1>
-              <span className='font-bold hidden md:block text-lg xl:text-xl'>🔗Archistock</span>
-              <span className='font-black block md:hidden text-xl text-center'>AS</span>
-            </h1>
-            <p className='text-xs hidden lg:block'>1.0.0</p>
-          </div>
-          <hr className="w-full h-0.5 mx-auto my-4 bg-gray-300 border-0 rounded" />
-          <AsideItem title='Your storage' icon={<Cloud size={20} weight="bold" />} link='storage' active={active === 'storage'} onClick={() => handleChangeItem('storage')}></AsideItem>
-          <AsideItem title='Extend storage' icon={<Lightning size={20} weight="bold" />} link='extend' active={active === 'extend'} onClick={() => handleChangeItem('extend')}></AsideItem>
-          <AsideItem title='Upload files' icon={<UploadSimple size={20} weight="bold" />} link='upload' active={active === 'upload'} onClick={() => handleChangeItem('upload')}></AsideItem>
-          <AsideItem title='Settings' icon={<Sliders size={20} />} link='settings' active={active === 'settings'} onClick={() => handleChangeItem('settings')}></AsideItem>
-          <AsideItem title='Components' icon={<Cube size={20} />} link='components' active={active === 'components'} onClick={() => handleChangeItem('components')}></AsideItem>
-          <AsideItem title='Profil' icon={<User size={20} />} link='profile' active={active === 'profile'} onClick={() => handleChangeItem('profile')}></AsideItem>
-          <AsideItem title="Logout" icon={<X size={20} />} link="/" active={active === 'logout'} onClick={() => handleLogout()} isLogout={true}></AsideItem>
+      <div className="flex flex-col my-4 mx-4">
+        <div className="title flex flex-row justify-center md:justify-between items-center">
+          <h1>
+            <span className="font-bold hidden md:block text-lg xl:text-xl">
+              🔗Archistock
+            </span>
+            <span className="font-black block md:hidden text-xl text-center">
+              AS
+            </span>
+          </h1>
+          <p className="text-xs hidden lg:block">1.0.0</p>
         </div>
+        <hr className="w-full h-0.5 mx-auto my-4 bg-gray-300 border-0 rounded" />
+        <AsideItem
+          title="Your storage"
+          icon={<Cloud size={20} weight="bold" />}
+          link="storage"
+          active={active === "storage"}
+          onClick={() => handleChangeItem("storage")}
+        ></AsideItem>
+        <AsideItem
+          title="Extend storage"
+          icon={<Lightning size={20} weight="bold" />}
+          link="extend"
+          active={active === "extend"}
+          onClick={() => handleChangeItem("extend")}
+        ></AsideItem>
+        <AsideItem
+          title="Upload files"
+          icon={<UploadSimple size={20} weight="bold" />}
+          link="upload"
+          active={active === "upload"}
+          onClick={() => handleChangeItem("upload")}
+        ></AsideItem>
+        <AsideItem
+          title="Settings"
+          icon={<Sliders size={20} />}
+          link="settings"
+          active={active === "settings"}
+          onClick={() => handleChangeItem("settings")}
+        ></AsideItem>
+        <AsideItem
+          title="Components"
+          icon={<Cube size={20} />}
+          link="components"
+          active={active === "components"}
+          onClick={() => handleChangeItem("components")}
+        ></AsideItem>
+        <AsideItem
+          title="Statistics"
+          icon={<ChartLine size={20} />}
+          link="statistics"
+          active={active === "statistics"}
+          onClick={() => handleChangeItem("statistics")}
+        ></AsideItem>
+        <AsideItem
+          title="Profil"
+          icon={<User size={20} />}
+          link="profile"
+          active={active === "profile"}
+          onClick={() => handleChangeItem("profile")}
+        ></AsideItem>
+        <AsideItem
+          title="Logout"
+          icon={<X size={20} />}
+          link="/"
+          active={active === "logout"}
+          onClick={() => handleLogout()}
+          isLogout={true}
+        ></AsideItem>
+      </div>
     </React.Fragment>
-  )
+  );
 }
