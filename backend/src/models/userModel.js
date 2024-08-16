1ddf4875bd32c92b53d6aa57b5221c5c4d564939
Damien <dlortiethibaut@gmail.com>
2024-08-16 10:29:34 +0200

    add fixtures

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,60 +1,75 @@
 const sequelize = require("../database/database");
 const { DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
 
 const Address = require("./addressModel");
 
 const User = sequelize.define(
-    "user",
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false
-          },
-        firstName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        lastName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        email: {
-            type: DataTypes.STRING,
-            unique: true,
-            allowNull: false,
-        },
-        phoneNumner: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        refreshToken: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        role: {
-            type: DataTypes.ENUM("admin, user"),
-            defaultValue: null,
-            allowNull: true,
-        },
-        addressId: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            references: {
-                model: Address,
-                key: "id",
-            },
-        },
+  "user",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
     },
-    {
-        sequelize,
-        freezeTableName: true,
-    }
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    phoneNumner: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    refreshToken: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    role: {
+      type: DataTypes.ENUM("admin, user"),
+      defaultValue: null,
+      allowNull: true,
+    },
+    addressId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: Address,
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    hooks: {
+      beforeCreate: async (user) => {
+        if (user.password) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      },
+      beforeUpdate: async (user) => {
+        if (user.password && user.changed("password")) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      },
+    },
+  }
 );
 
 module.exports = User;
@@ -62,4 +77,3 @@ module.exports = User;
 //relations one user have one adress
 User.belongsTo(Address, { foreignKey: "addressId", as: "address" });
 Address.hasOne(User, { foreignKey: "addressId", as: "user" });
-
